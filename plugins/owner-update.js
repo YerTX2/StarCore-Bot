7cc2d3a629784385e6dd723d1166860903d623ff
Jostin-444 <gemersport7@gmail.com>
2024-11-18 21:51:00 -0600

    Add files via upload

--- plugins/owner-update.js
+++ plugins/owner-update.js
@@ -1,10 +1,12 @@
+
 import { execSync } from 'child_process'
 let handler = async (m, { conn, text }) => {
-await m.react('🕓')
+
+    m.react(done)
 if (conn.user.jid == conn.user.jid) {
 let stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''))
-await conn.reply(m.chat, stdout.toString(), m)
-await m.react('✅')
+//require('fs').readdirSync('plugins').map(v=>global.reload('', v))
+conn.reply(m.chat, stdout.toString(), m)
 }}
 handler.help = ['update']
 handler.tags = ['owner']
