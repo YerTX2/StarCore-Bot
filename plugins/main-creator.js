6becd28fbdbf23a33a101ca5d74810dc4670bb56
Niño Piña <ninopina10@gmail.com>
2024-11-19 19:47:16 -0600

    Actualizar main-creator.js

--- plugins/main-creator.js
+++ plugins/main-creator.js
@@ -1,12 +1,12 @@
 function handler(m) {
-let name = conn.getName('51907376960@s.whatsapp.net')
-let ownerN = '50557865603'
+let name = conn.getName('50557865603@s.whatsapp.net')
+let ownerN = '51907376960'
 conn.sendContact(m.chat, [[`${ownerN}@s.whatsapp.net`, `${name}`]], m, {
  contextInfo: { 
  forwardingScore: 2023,
 isForwarded: false, 
  externalAdReply: {  
- title: `¡Hi! This is my developer's number, just shoot me a message if you have any questions or want to chat about a potential collab.`, 
+ title: `¡Hl! Este Es El Número De Mi Creador`, 
  body: botName, 
  sourceUrl: 'https://whatsapp.com/channel/0029VankMyeBadmR9Ou0So3t',
  thumbnail: imagen,
