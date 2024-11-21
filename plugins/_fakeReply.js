c1414a291a061318347f54b94325c60fd419a2f8
ZxYerSixZx <oroscoyerson2019@gmail.com>
2024-11-21 17:13:43 -0500

    Actualizar _fakeReply.js

--- plugins/_fakeReply.js
+++ plugins/_fakeReply.js
@@ -1,36 +1,36 @@
-import fetch from 'node-fetch'
-
+import fetch from 'node-fetch';
 export async function before(m, { conn }) {
-let img = await (await fetch(`https://tinyurl.com/299lyvo5`)).buffer()
+   let pp = await this.profilePictureUrl(m.sender, 'image').catch(_ => 'https://i.ibb.co/1ZxrXKJ/avatar-contact.jpg');
 
- global.rcanal = {
+  // Respuesta con enlace de WhatsApp
+  global.rpl = {
     contextInfo: {
-            isForwarded: true,
-    forwardedNewsletterMessageInfo: {
-      newsletterJid: "120363350628883149@newsletter",
-      serverMessageId: 100,
-      newsletterName: namechannel,
-    },
-    },
-  }
+      externalAdReply: {
+        mediaUrl: link_,
+        mediaType: 'VIDEO',
+        description: 'support group',
+        title: packname,
+        body: 'grupo de soporte',
+        thumbnailUrl: pp,
+        sourceUrl: link_
+      }
+    }
+  };
 
- global.adReply = {
-            contextInfo: { 
-             forwardingScore: 9999, 
-                 isForwarded: false, 
-                    externalAdReply: {
-                                    showAdAttribution: true,
-                                        title: botname,
-                                        body: textbot,
-                                        mediaUrl: null,
-                                        description: null,
-                                        previewType: "PHOTO",
-                                        thumbnailUrl: img,
-                    thumbnail: img,
-                           sourceUrl: canal,
-                           mediaType: 1,
-                   renderLargerThumbnail: true
-                                }
-                        }
-                }
+ global.fkontak = {
+        key: {
+            participant: '0@s.whatsapp.net',
+            remoteJid: 'status@broadcast'
+        },
+        message: {
+            contactMessage: {
+                displayName: `${global.db.data.users[m.sender].name}`,
+                vcard: `BEGIN:VCARD\nVERSION:3.0\nN:XL;${botName},;;;\nFN:${botName},\nitem1.TEL;waid=${m.sender.split("@")[0]}:${m.sender.split("@")[0]}\nitem1.X-ABLabell:Ponsel\nEND:VCARD`,
+                jpegThumbnail: imagen0,
+                thumbnail: imagen0,
+                sendEphemeral: true
+            }
+        }
+    }
+
 }
