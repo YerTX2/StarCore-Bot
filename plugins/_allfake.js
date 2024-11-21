6ef23c5f0069fd18b572534a611ac329a8115671
ZxYerSixZx <oroscoyerson2019@gmail.com>
2024-11-21 17:05:48 -0500

    Actualizar plugins

---
+++ plugins/_allfake.js
@@ -0,0 +1,48 @@
+import fetch from 'node-fetch'
+
+export async function before(m, { conn }) {
+//let img = await (await fetch(`https://tinyurl.com/299lyvo5`)).buffer()
+let img = catalogo
+ global.rcanal = {
+    contextInfo: {
+            isForwarded: true,
+    forwardedNewsletterMessageInfo: {
+      newsletterJid: "120363350628883149@newsletter",
+      serverMessageId: 100,
+      newsletterName: '【★】𝚃𝚎𝚊𝚖  乂   𝐴𝑁𝐺  乂 𝐶𝐻𝐴𝑁𝑁𝐸𝐿  𝑂𝑓𝑐【★】', 
+    },
+            externalAdReply: {
+                                    showAdAttribution: true,
+                                        title: botname,
+                                        body: 'Hola',
+                                        mediaUrl: null,
+                                        description: null,
+                                        previewType: "PHOTO",
+                                        thumbnailUrl: 'https://telegra.ph/file/8813c9636643198e783fd.jpg',
+                           sourceUrl: canal,
+                           mediaType: 1,
+                   renderLargerThumbnail: false
+            },
+    },
+  }
+
+ global.adReply = {
+            contextInfo: { 
+             forwardingScore: 9999, 
+                 isForwarded: false, 
+                    externalAdReply: {
+                                    showAdAttribution: true,
+                                        title: botname,
+                                        body: textbot,
+                                        mediaUrl: null,
+                                        description: null,
+                                        previewType: "PHOTO",
+                                        thumbnailUrl: img,
+                    thumbnail: img,
+                           sourceUrl: canal,
+                           mediaType: 1,
+                   renderLargerThumbnail: true
+                                }
+                        }
+                }
+}
