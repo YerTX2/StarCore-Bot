2244f694e174c6e2802c12464870f66c7142b316
Niño Piña <ninopina10@gmail.com>
2024-11-22 12:39:36 -0600

    Actualizar _fakeReply.js

--- plugins/_fakeReply.js
+++ plugins/_fakeReply.js
@@ -2,126 +2,35 @@ import fetch from 'node-fetch';
 export async function before(m, { conn }) {
    let pp = await this.profilePictureUrl(m.sender, 'image').catch(_ => 'https://i.ibb.co/1ZxrXKJ/avatar-contact.jpg');
 
-  let nam = "✨  STARCORE - CHANNEL  ✨"
-
   // Respuesta con enlace de WhatsApp
   global.rpl = {
     contextInfo: {
-            isForwarded: true,
-    forwardedNewsletterMessageInfo: {
-      newsletterJid: id_canal,
-      serverMessageId: 100,
-      newsletterName: nam,
-    }, 
       externalAdReply: {
-        mediaUrl: bgp,
+        mediaUrl: link_,
         mediaType: 'VIDEO',
         description: 'support group',
         title: packname,
         body: 'grupo de soporte',
         thumbnailUrl: pp,
-        sourceUrl: bgp
+        sourceUrl: link_
       }
     }
   };
 
-  // Respuesta con enlace de Canal de WhatsApp
-  global.rcanal = {
-    contextInfo: {
-            isForwarded: true,
-    forwardedNewsletterMessageInfo: {
-      newsletterJid: id_canal,
-      serverMessageId: 100,
-      newsletterName: nam,
-    }, 
-      externalAdReply: {
-        mediaUrl: fgcanal,
-        mediaType: 'VIDEO',
-        description: 'canal del grupo',
-        title: packname,
-        body: 'Canal Oficial',
-        thumbnailUrl: pp,
-        sourceUrl: fgcanal
-      }
+ global.fkontak = {
+        key: {
+            participant: '0@s.whatsapp.net',
+            remoteJid: 'status@broadcast'
+        },
+        message: {
+            contactMessage: {
+                displayName: `${global.db.data.users[m.sender].name}`,
+                vcard: `BEGIN:VCARD\nVERSION:3.0\nN:XL;${botName},;;;\nFN:${botName},\nitem1.TEL;waid=${m.sender.split("@")[0]}:${m.sender.split("@")[0]}\nitem1.X-ABLabell:Ponsel\nEND:VCARD`,
+                jpegThumbnail: imagen0,
+                thumbnail: imagen0,
+                sendEphemeral: true
+            }
+        }
     }
-  }
-
-    // Forwarded
-  global.fwc = {
-    contextInfo: {
-            isForwarded: true,
-    forwardedNewsletterMessageInfo: {
-      newsletterJid: id_canal,
-      serverMessageId: 100,
-      newsletterName: nam,
-    }
-    }
-  }
-
-  // Respuesta con enlace de PayPal
-  global.rpyp = {
-    contextInfo: {
-            isForwarded: true,
-    forwardedNewsletterMessageInfo: {
-      newsletterJid: id_canal,
-      serverMessageId: 100,
-      newsletterName: nam,
-    }, 
-      externalAdReply: {
-        mediaUrl: fgpyp,
-        mediaType: 'VIDEO',
-        description: 'Donate',
-        title: 'PayPal',
-        body: 'ayuda a mantener el bot activo',
-        thumbnailUrl: pp,
-        sourceUrl: fgpyp
-      }
-    }
-  };
-
-  // Respuesta con enlace de Instagram
-  global.rpig = {
-    contextInfo: {
-            isForwarded: true,
-    forwardedNewsletterMessageInfo: {
-      newsletterJid: id_canal,
-      serverMessageId: 100,
-      newsletterName: nam,
-    }, 
-      externalAdReply: {
-        mediaUrl: fgig,
-        mediaType: 'VIDEO',
-        description: 'Sigueme por Instagram',
-        title: 'Instagram',
-        body: 'sigueme por Instagram',
-        thumbnailUrl: pp,
-        sourceUrl: fgig
-      }
-    }
-  };
-
-  // Respuesta con enlace de YouTube
-  global.rpyt = {
-    contextInfo: {
-            isForwarded: true,
-    forwardedNewsletterMessageInfo: {
-      newsletterJid: id_canal,
-      serverMessageId: 100,
-      newsletterName: nam,
-    }, 
-      externalAdReply: {
-        showAdAttribution: true,
-        mediaUrl: fgyt,
-        mediaType: 'VIDEO',
-        description: 'Suscribete: ' + fgyt,
-        title: 'SCA YouTube',
-        body: 'aprende a crear tus propios bots',
-        thumbnailUrl: pp,
-        sourceUrl: fgyt
-      }
-    }
-  }
-
-
 
 }
