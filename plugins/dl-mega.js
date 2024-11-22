28bb4a4b283f113ca251581a80f731d5e367dcd8
ZxYerSixZx <oroscoyerson2019@gmail.com>
2024-11-22 05:16:39 -0500

    Actualizar dl-mega.js

--- plugins/dl-mega.js
+++ plugins/dl-mega.js
@@ -1,53 +1,48 @@
+
+
+```
 import { File } from "megajs";
 import path from "path";
 
 let handler = async (m, { conn, args, usedPrefix, text, command }) => {
-    try {
-        if (!text) return conn.reply(m.chat, `\`\`\`[ 🌴 ] Uso correcto del comando:\`\`\` ${usedPrefix + command} https://mega.nz/file/ovJTHaQZ#yAbkrvQgykcH_NDKQ8eIc0zvsN7jonBbHZ_HTQL6lZ8`, null, { quoted: fkontak});
-
-        const file = File.fromURL(text);
-        await file.loadAttributes();
-
-        if (file.size >= 300000000) return m.reply('Error: El archivo es demasiado pesado (Peso máximo: 300MB ( Premium: 800MB )');
-
-        m.react(rwait);
-
-        const caption = `   *--- ${botName} ---*\nFile: ${file.name}\nSize: ${formatBytes(file.size)}`;
-
-        const data = await file.downloadBuffer();
-
-        const fileExtension = path.extname(file.name).toLowerCase();
-        const mimeTypes = {
-            ".mp4": "video/mp4",
-            ".pdf": "application/pdf",
-            ".zip": "application/zip",
-            ".rar": "application/x-rar-compressed",
-            ".7z": "application/x-7z-compressed",
-            ".jpg": "image/jpeg",
-            ".jpeg": "image/jpeg",
-            ".png": "image/png",
-        };
-
-        let mimetype = mimeTypes[fileExtension] || "application/octet-stream";
-
-        await conn.sendFile(m.chat, data, file.name, caption, m, null, { mimetype, asDocument: true });
-
-    } catch (error) {
-        return m.reply(`Error: ${error.message}`);
-    }
+  try {
+    if (!text) return conn.reply(m.chat, `\`\`\`[ 🌴 ] Uso correcto del comando:\`\`\` ${usedPrefix + command} (enlace no disponible), null, { quoted: fkontak});
+    const file = File.fromURL(text);
+    await file.loadAttributes();
+    if (file.size >= 300000000) return m.reply('Error: El archivo es demasiado pesado (Peso máximo: 300MB ( Premium: 800MB )');
+    m.react(rwait);
+    const caption = ` *--- ${botName} ---*\nFile: ${file.name}\nSize: ${formatBytes(file.size)}`;
+    const data = await file.downloadBuffer();
+    const fileExtension = path.extname(file.name).toLowerCase();
+    const mimeTypes = {
+      ".mp4": "video/mp4",
+      ".pdf": "application/pdf",
+      ".zip": "application/zip",
+      ".rar": "application/x-rar-compressed",
+      ".7z": "application/x-7z-compressed",
+      ".jpg": "image/jpeg",
+      ".jpeg": "image/jpeg",
+      ".png": "image/png",
+    };
+    let mimetype = mimeTypes[fileExtension] || "application/octet-stream";
+    let imagen = await conn.getBuffer(data);
+    await conn.sendMessage(m.chat, { image: imagen, caption: `@${m.sender.split('@')[0]}`, contextInfo: { mentionedJid: [m.sender], isForwarded: true, forwardingScore: 1, forwardedNewsletterMessageInfo: { newsletterJid: '120363350628883149@newsletter', newsletterName: '【✫𝚃𝙴𝙰𝙼 乂 𝚂𝚃𝙰𝚁𝙲𝙾𝚁𝙴✫】', serverMessageId: -1 }}}, { quoted: fkontak});
+  } catch (error) {
+    return m.reply(`Error: ${error.message}`);
+  }
 }
 
 handler.help = ["mega"]
 handler.tags = ["descargasStarcore"]
 handler.command = /^(mega)$/i
+
 export default handler
 
 function formatBytes(bytes) {
-    if (bytes === 0) return '0 Bytes';
-
-    const k = 1024;
-    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
-    const i = Math.floor(Math.log(bytes) / Math.log(k));
-
-    return parseFloat((bytes / Math.pow(k, i)).toFixed(2)) + ' ' + sizes[i];
+  if (bytes === 0) return '0 Bytes';
+  const k = 1024;
+  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
+  return parseFloat((bytes / Math.pow(k, i)).toFixed(2)) + ' ' + sizes[i];
 }
+```
