4a4190c01616a44ce56efcca37eb7c5a3e6aff7d
Niño Piña <ninopina10@gmail.com>
2024-11-23 11:19:58 -0600

    Actualizar menuhelpStarCore.js

--- plugins/menuhelpStarCore.js
+++ plugins/menuhelpStarCore.js
@@ -22,7 +22,7 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname, args, command}) => {
 let tags = {
 'main': 'menu',
 }
-let img = 'https://i.ibb.co/twRmk54/StarCore.jpg'
+let img = 'https://i.ibb.co/0VqVr62/StarCore.jpg'
 
   try {
           // DEFAULT MENU
