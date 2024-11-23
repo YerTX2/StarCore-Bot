74e7709a5c7ef6798052fb24f54b7b8a650888d8
Niño Piña <ninopina10@gmail.com>
2024-11-23 09:36:59 -0600

    Actualizar menuhelpStarCore.js

--- plugins/menuhelpStarCore.js
+++ plugins/menuhelpStarCore.js
@@ -173,7 +173,7 @@ let video = 'https://files.catbox.moe/f0q0he.mp4'
     text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
 
  let fkon = { key: { fromMe: false, participant: `${m.sender.split`@`[0]}@s.whatsapp.net`, ...(m.chat ? { remoteJid: '16504228206@s.whatsapp.net' } : {}) }, message: { contactMessage: { displayName: `${name}`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}}
-await m.react('🔱') 
+await m.react('🤖') 
 
  conn.sendMessage(m.chat, {
         text: text,
