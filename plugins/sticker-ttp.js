a2b865de9bb86ac088bc469ec001450160560359
Niño Piña <ninopina10@gmail.com>
2024-11-23 08:43:03 -0600

    Actualizar sticker-ttp.js

--- plugins/sticker-ttp.js
+++ plugins/sticker-ttp.js
@@ -11,7 +11,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     throw stiker.toString()
 }
 handler.help = ['ttp <text>']
-handler.tags = ['sticker']
+handler.tags = ['tools']
 handler.command = ['ttp']
 
 export default handler
