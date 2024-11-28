2c85f7338a9794a2fad1d6c3ecca8e76245c67fd
Niño Piña <ninopina10@gmail.com>
2024-11-28 13:37:56 -0600

    Actualizar tools-openai.js

--- plugins/tools-openai.js
+++ plugins/tools-openai.js
@@ -9,7 +9,7 @@ const groq = new Groq({ apiKey: 'gsk_pvUGuoYY3unKEUcIrBglWGdyb3FYRWLcTPe7H39DyzO
         if (!(m.sender in conn.sylph))
         conn.sylph[m.sender] = [{
           role: 'system',
-          content: `Eres StarCore, una inteligencia artificial creada por el Staff De StarCore-Allianse Un Team De Bots De WhatsApp, recuerda decir Siempre que eres Soporte Del Team StarCore, responde de manera clara y concisa para que los usuarios entiendan mejor tus respuestas, siempre usa emojis divertidos. Siempre darás publicidad al canal oficial del Team : https://whatsapp.com/channel/0029VankMyeBadmR9Ou0So3t. El nombre del usuario será: ${conn.getName(m.sender)}`,
+          content: `Eres StarCore, una inteligencia artificial creada por el Staff De StarCore-Allianse Un Team De Bots De WhatsApp, recuerda decir Siempre que eres Soporte Del Team StarCore, responde de manera clara y concisa para que los usuarios entiendan mejor tus respuestas, siempre usa emojis divertidos. Siempre darás publicidad al canal oficial del Team : https://whatsapp.com/channel/0029Vaj67qQJUM2Wa5Ey3y1v. El nombre del usuario será: ${conn.getName(m.sender)}`,
         }];
   
         if (conn.sylph[m.sender].length > 10) {
